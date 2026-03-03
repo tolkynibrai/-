0286febbe8275fdbbbe475b7cd7e66b90ca2ac26
lector <lector_225@mail.ru>
2026-03-03 23:27:33 +0500

    массивы

--- script.js
+++ script.js
@@ -585,3 +585,137 @@
 //     console.log(`Зп ${key} составляет ${engineers[key]} usd.`) 
 // }
 
+//Массивы в js
+
+// const numbers=[1, 2, 3, 4, 5]
+// const names= ['Kairat','Altair','Assel','Tolkyn']
+
+// console.log(numbers)
+// console.log(names) //index=>0, 1, 2, 3 индекс
+
+// console.log(names[0])
+
+// names[3]='Dinis'
+// names[2]='Kirill'
+// console.log(names)
+
+//добавить в конец массива
+
+// names.push('Aisultan')
+// console.log(names)
+
+
+// names.unshift('liliya')
+// console.log(names)
+
+//Удалить элемент из массива последний эллемент
+
+// names.pop()
+// console.log(names)
+
+//Удалить элемент из массива первый элемент
+
+// names.shift()
+// console.log(names)
+
+//мутабельный push pop unshift shift= меняет ориг массив
+
+//не мутабельный 
+
+// names.forEach()
+
+
+// for(let i=0; i<names.length; i++){
+//   console.log(names[i])
+// } //чтобы не писать число каждый раз и функия будет подстраиватся на число в массиве 
+ 
+// for(const index in names){
+//   console.log(index, names[index])
+// }
+
+// for (const value of names){
+//   console.log(value)
+// }
+
+// names.forEach ((value) =>{
+// console.log(value)
+// })
+
+// const fruits =[ 'apple','banana', 'pineapple', 'orange','grape','lemon','lome']
+
+
+// for (let i=1; i< fruits.length; i+=2){
+//   console.log(fruits[i])
+// }
+
+// const numbers =[ 42, 65, 49, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
+
+// for(let i=0; i<numbers.length; i++){
+//   console.log(`индeксу ${i} соответсвует число ${numbers[i]}`)
+// }
+
+// Задание 6
+
+// Есть массив произвольных чисел:
+// const numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
+
+// 1) Вывести в консоль все элементы массива, используя 2 разных цикла:
+// for-of и for со счетчиком
+
+// 2) Посчитать и вывести в консоль сумму элементов в массиве.
+
+// Подсказка: в задачах такого типа нужно заранее предусмотреть
+// переменную (например, sum) и записать в нее начальное значение
+// (в случае с суммой - ноль). Затем уже описать цикл,
+// в котором эта сумма будет считаться (в каждой итерации цикла к ней будет
+//     прибавляться следующий элемент массива).
+
+// 3) Посчитать и вывести в консоль сумму только четных чисел в массиве.
+
+// 4) Найти и вывести в консоль максимальное число массива.
+
+// Подсказка: как и в случае с суммой, здесь сначала нужно объявить
+// переменную (например, max), записать в нее первый элемент массива,
+// а затем уже в цикле искать максимальное значение.
+
+// 5) Определить и вывести в консоль индекс максимального числа массива
+// (или индексы, если число встречается более одного раза).
+// Само максимальное число мы уже нашли в прошлой части задачи, повторно его искать не нужно.
+
+
+
+const numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
+
+// for(const number of numbers){
+//   console.log(number)
+// }
+
+// for(let i=0; i<numbers.length; i++){
+//   console.log(numbers[i])
+// }
+
+// let sum= 0
+// for(const number of numbers){
+//   sum=sum + number
+// console.log(sum)
+// }
+
+// let sum= 0
+//  for(const number of numbers){
+//   if(number% 2 ===0)
+//   sum+= number
+// }
+//   console.log(sum)
+
+let max=0
+
+for(const number of numbers){
+  if (max < number) {
+    console.log('старое максимальное число,', max)
+    max= number 
+    console.log('новое максимальное число,', max)
+  }
+  else{
+    console.log(`число ${number} меньше чем максимальное число ${max} ничего не делаем`)
+  }
+}
