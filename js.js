4f899ee473ba4587ecc1ecc8668dda6ea6d37e0a
lector <lector_225@mail.ru>
2026-03-25 15:08:08 +0500

    dom

---
+++ js.js
@@ -0,0 +1,65 @@
+//Dom модель обьектов браузера
+
+// console.log(location)
+// console.log(location.href)
+
+// location.replace('https://www.youtube.com/watch?v=xddAnDQdJ-s')
+
+
+//Novigator
+// console.log(navigator)
+// console.log(navigator.geolocation.getCurrentPosition)
+
+//history
+// console.log(history)
+
+//screen
+// console.log(screen)
+
+// console.log(document.querySelector('h1'))
+// console.dir(document.querySelector('h1'))//информация об элементе
+// console.dir(document.querySelectorAll('h1'))
+
+// console.dir(document.querySelector('.link'))
+// console.dir(document.querySelector('#heaging1'))
+
+
+// console.dir(document.getElementById('#heading2'))
+
+// const heading1= document.querySelector('#heading1')
+// console.dir( heading1.innerText)
+// heading1.innerText= 'sagynysh'
+
+// const listItimes= document.querySelectorAll('#list li')
+// console.log(listItimes)
+
+// for( const li of listItimes){
+//   console.log(li.innerText)
+// }
+
+// let count= 0
+// for( const li of listItimes){
+//   li.innerText= count
+//   count ++
+// }
+// let count= 0
+// for(let i=0; i<listItimes.length; i++){
+//   listItimes[i].innerText= i
+// }
+
+//Удаление эллементов
+
+// const a= document.querySelector('.link')
+// console.log(a)
+// a.remove()
+
+// const list=document.querySelector('#list')
+// list.removeChild(a)
+// a.remove()
+
+// const elementsToRemove = document.querySelectorAll('.forRemove');
+
+// for( const el of elementsToRemove){
+//   el.remove();
+// }
+
